44a572f80d3ce639e110a3c8ba1899d513bd393d
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2025-01-03 21:35:40 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -3,6 +3,11 @@
    $db_name = 'pgsql:host=ep-empty-term-a5e8u3sf-pooler.us-east-2.aws.neon.tech;dbname=neondb';
    $user_name = 'neondb_owner';
    $user_password = '73BcQNWkrHiS';
-   $conn = new PDO($db_name, $user_name, $user_password);
-   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   try {
+      $conn = new PDO($db_name, $user_name, $user_password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Oke"
+         } catch (PDOException $e) {
+$e->getMessage()
+   }
 ?>
