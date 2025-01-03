abf9675da65dd6a9e80a9d9e195a8302eb1aa1d3
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2025-01-04 02:10:03 +0700

    Update user_header.php

--- components/user_header.php
+++ components/user_header.php
@@ -39,7 +39,7 @@ if(isset($message)){
 
       <div class="profile">
          <?php
-            $select_profile = $conn->prepare('SELECT * FROM "users" WHERE id = ?');
+            $select_profile = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
             $select_profile->execute([$user_id]);
             if($select_profile->rowCount() > 0){
                $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
