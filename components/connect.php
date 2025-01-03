34c6dfc9344f6c92c008f6107274596b95b1eb29
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2025-01-03 21:30:20 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,6 +1,6 @@
 <?php
 
-   $db_name = 'mysql:host=ep-empty-term-a5e8u3sf-pooler.us-east-2.aws.neon.tech;dbname=neondb';
+   $db_name = 'pgsql:host=ep-empty-term-a5e8u3sf-pooler.us-east-2.aws.neon.tech;dbname=neondb';
    $user_name = 'neondb_owner';
    $user_password = '73BcQNWkrHiS';
    $conn = new PDO($db_name, $user_name, $user_password);
