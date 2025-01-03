067b591d4a47ac45ca8bd642f279023adf933e44
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2025-01-03 21:25:57 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,8 +1,8 @@
 <?php
 
-   $db_name = 'mysql:host=localhost;dbname=blog_db';
-   $user_name = 'root';
-   $user_password = '';
+   $db_name = 'mysql:host=ep-empty-term-a5e8u3sf-pooler.us-east-2.aws.neon.tech;dbname=neondb';
+   $user_name = 'neondb_owner';
+   $user_password = '73BcQNWkrHiS';
    $conn = new PDO($db_name, $user_name, $user_password);
    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
