da655e512274b2dd283e3797dbc54467825c4f94
hdiiofficial <79190751+hdiiofficial@users.noreply.github.com>
2025-01-04 02:08:15 +0700

    Update connect.php

--- components/connect.php
+++ components/connect.php
@@ -1,5 +1,5 @@
 <?php
-   $db_name = 'mysql:host=localhost;dbname=sql12755504';
+   $db_name = 'mysql:host=sql12.freemysqlhosting.net;dbname=sql12755504';
    $user_name = 'sql12755504';
    $user_password = 'qsZRBT5StY';
    $conn = new PDO($db_name, $user_name, $user_password);
